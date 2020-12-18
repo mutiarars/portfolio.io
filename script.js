79cd871b6e67c2616d96d72a29e2857cf727b810
mutiarars <mutiarars16@gmail.com>
2020-12-18 15:49:20 +0700

    my project

---
+++ script.js
@@ -0,0 +1,18 @@
+$('.home').click(function(e){
+    $(document).scrollTop(100)
+  });
+
+ $('.about').click(function(e){
+    $(document).scrollTop(150)
+  });
+
+  $('.skillsnav').click(function(e){
+    $(document).scrollTop(700)
+  });
+
+  $('.explore').click(function(e){
+    $(document).scrollTop(1200)
+  });
+  $('.contactnav').click(function(e){
+    $(document).scrollTop(5000)
+  });
